27b726a82b9d04a9458845424e6a983dce536481
leonmhl <leonmoehl@outlook.de>
2023-12-19 21:11:35 +0200

    Week14

---
+++ Week14/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Dog goodBoy = new Dog("Good Boy");
+Console.WriteLine($"Name: {goodBoy.Name}; Level of happiness: {goodBoy.LevelOfHappinness}");
+
+while (goodBoy.LevelOfHappinness < 5)
+{
+    goodBoy.Bark();
+}
+
+goodBoy.Wag();
+
+
+
+class Dog
+{
+    public string Name;
+    public int LevelOfHappinness;
+
+    public Dog(string name)
+    {
+        Name = name;
+        LevelOfHappinness = 0;
+        Console.WriteLine($"{Name} is your friend now.");
+    }
+
+    public void Bark()
+    {
+        LevelOfHappinness++;
+        Console.WriteLine("Woof-woof");
+    }
+
+    public void Wag()
+    {
+        Console.WriteLine($"{Name} starts wagging its tail.");
+        Console.WriteLine("Whoosh-Whoosh");
+    }
+}
